7594ceae7f2d9bdc86e50662086a4e1cd0f16d7f
Daniel Ye <danielxinxin1@gmail.com>
2020-12-16 22:36:03 -0300

    adicionando metodos do controller

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,50 @@
 package controller;
 
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import service.UserService;
+
+import java.util.Optional;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity listAll(){
+        return new ResponseEntity(service.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity saveUser(@RequestBody User user) {
+        User added = service.add(user);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity searchUser(@PathVariable("id") String UserId) {
+        Optional<User> User = service.findById(UserId);
+        if (User.isPresent()) {
+            return new ResponseEntity<User>(User.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity updateUser(@PathVariable("id") String id, @RequestBody User User) {
+        if (service.update(id, User)) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
 }
