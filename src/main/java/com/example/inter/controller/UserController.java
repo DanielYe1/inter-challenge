923c7cd0286719278d4cd3cfeb9bfb5ce450ca87
Daniel Ye <danielxinxin1@gmail.com>
2020-12-21 18:22:21 -0300

    adicionando correcoes no controller; adicionando lib sunfire maven

--- src/main/java/com/example/inter/controller/UserController.java
+++ src/main/java/com/example/inter/controller/UserController.java
@@ -27,42 +27,42 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity saveUser(@RequestBody UserDTO user) {
         User added = userService.add(user);
-        return new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity<>(added, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity searchUser(@PathVariable("id") String userId) {
         Optional<User> user = userService.findById(userId);
         if (user.isPresent()) {
-            return new ResponseEntity<User>(user.get(), HttpStatus.OK);
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
         } else {
-            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity updateUser(@PathVariable("id") String id, @RequestBody UserDTO user) {
         if (userService.update(id, user)) {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity deleteUser(@PathVariable("id") String id) {
         userService.delete(id);
-        return new ResponseEntity((HttpStatus.NO_CONTENT));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(value = "/check", method = RequestMethod.POST)
     public ResponseEntity calculateCheckDigit(@RequestBody RequestDTO digitValues) {
         CheckDigit checkDigit = userService.addCheckDigitToUser(digitValues.getUserId(), digitValues.getN(), digitValues.getK());
-        return new ResponseEntity(checkDigit, HttpStatus.OK);
+        return new ResponseEntity<>(checkDigit, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/key")
     public ResponseEntity addUserKey(@RequestBody UserKey userKey) {
-        return new ResponseEntity(userService.saveUserKey(userKey), HttpStatus.OK);
+        return new ResponseEntity<>(userService.saveUserKey(userKey), HttpStatus.OK);
     }
 }
