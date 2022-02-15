f1a6d069b728d0c35fc14b56defe5ef7d0751a14
Daniel Ye <danielxinxin1@gmail.com>
2022-02-15 00:27:27 -0300

    adding integration tests

---
+++ src/test/java/com/example/inter/integration/IntegrationTests.java
@@ -0,0 +1,43 @@
+package com.example.inter.integration;
+
+import com.example.inter.controller.UserController;
+import com.example.inter.repository.PublicKeyRepository;
+import com.example.inter.service.SecurityService;
+import com.example.inter.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc
+public class IntegrationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private PublicKeyRepository publicKeyRepository;
+
+    @MockBean
+    private SecurityService securityService;
+
+    @Test
+    public void returns_200_for_user_endpoint() throws Exception {
+        this.mockMvc.perform(get("/user/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk());
+    }
+}
